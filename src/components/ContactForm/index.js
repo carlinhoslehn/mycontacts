ad6a2f3b2ef1c00caf29ec58ea9c9a18ba998988
Carlos Eduardo <dudunh@gmail.com>
2023-09-03 18:37:43 -0300

    Telas: Home, Novo, Editar

---
+++ src/components/ContactForm/index.js
@@ -0,0 +1,41 @@
+import PropTypes from 'prop-types';
+
+import Input from '../Input';
+import Select from '../Select';
+import Button from '../Button';
+
+import FormGroup from '../FormGroup';
+import { ButtonContainer, Form } from './style';
+
+export default function ContactForm({ buttonLabel }) {
+  return (
+    <Form>
+      <FormGroup>
+        <Input type="text" placeholder="Nome" />
+      </FormGroup>
+
+      <FormGroup>
+        <Input type="text" placeholder="E-mail" />
+      </FormGroup>
+      <FormGroup>
+        <Input type="text" placeholder="Telefone" />
+      </FormGroup>
+      <FormGroup>
+        <Select>
+          <option value="Instagram">Instagram</option>
+          <option value="Facebook">Facebook</option>
+        </Select>
+      </FormGroup>
+      <ButtonContainer>
+        <Button type="submit">
+          {buttonLabel}
+        </Button>
+      </ButtonContainer>
+
+    </Form>
+  );
+}
+
+ContactForm.propTypes = {
+  buttonLabel: PropTypes.string.isRequired,
+};
---
+++ src/pages/Home/index.js
@@ -0,0 +1,52 @@
+import { Link } from 'react-router-dom';
+import {
+  Container, Header, ListContainer, Card, InputSearchContainer,
+} from './style';
+import arrow from '../../assets/images/icons/arrow.svg';
+import edit from '../../assets/images/icons/edit.svg';
+import trash from '../../assets/images/icons/trash.svg';
+
+export default function Home() {
+  return (
+    <Container>
+      <InputSearchContainer>
+        <input type="text" placeholder="Search" />
+      </InputSearchContainer>
+      <Header>
+        <strong>3 Contatos</strong>
+        <Link to="/new">Novo Contato</Link>
+      </Header>
+
+      <ListContainer>
+        <header>
+          <button type="button" className="sort-button">
+            <span>Nome</span>
+            <img src={arrow} alt="Arrow" />
+          </button>
+        </header>
+
+        <Card>
+          <div className="info">
+            <div className="contact-name">
+              <strong>Carlos eduardo</strong>
+              <small>Instagram</small>
+            </div>
+            <span>email@email.combr</span>
+            <span>(51) 0000000000</span>
+          </div>
+
+          <div className="actions">
+            <Link to="/edit/123">
+              <img src={edit} alt="Edit" />
+            </Link>
+
+            <button type="button">
+              <img src={trash} alt="Delete" />
+            </button>
+          </div>
+        </Card>
+
+      </ListContainer>
+    </Container>
+  );
+}
